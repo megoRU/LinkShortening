58c231f1867468d1b05958402f2292ee41ee3967
mego <savin.yura2014@gmail.com>
2021-02-17 17:19:13 +0300

    Version: 0.2.1 1. Change password DB 2. Fix pom

--- src/main/java/com/LinkShortening/controller/MainController.java
+++ src/main/java/com/LinkShortening/controller/MainController.java
@@ -18,7 +18,7 @@ public class MainController {
   private MessageRepo messageRepo;
 
   @GetMapping("/")
-  public String greeting(Map<String, Object> model) {
+  public String site(Map<String, Object> model) {
     return "site";
   }
 
@@ -47,18 +47,4 @@ public class MainController {
     return "main";
   }
 
-//  @PostMapping("filter")
-//  public String filter(@RequestParam String filter, Map<String, Object> model) {
-//    Iterable<Message> messages;
-//
-//    if (filter != null && !filter.isEmpty()) {
-//      messages = messageRepo.findByTag(filter);
-//    } else {
-//      messages = messageRepo.findAll();
-//    }
-//
-//    model.put("messages", messages);
-//
-//    return "main";
-//  }
 }
